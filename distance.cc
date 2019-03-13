a637331a66b2068dc1847ead36667c938375a7ac
Laurabelle <laurabellek@yahoo.com>
2019-03-12 17:45:23 -0700

    non-ip

--- distance.cc
+++ distance.cc
@@ -1,29 +1,16 @@
 #include "cache.h"
 
-#define TABLE_COUNT 16
-#define IP_COUNT 64
-#define PREFETCH_DEGREE 1
+#define TABLE_COUNT 1024
 
-//FIXME: Change how the table is stored per IP
 typedef struct distance_table
 {
-    int distance;
+    int tag;
+    int d1;
+    int d2;
+    int lru;
 } distance_table_t;
 
-typedef struct ip_table
-{
-    uint64_t ip;
-    distance_table_t tables[TABLE_COUNT];
-    int distance;
-    int previous_d;
-    int previous_i;
-    uint64_t previous_a;
-    int lru;
-    int fr;
-}ip_table_t;
-
-ip_table_t iptable[IP_COUNT];
-//distance_table_t tables[TABLE_COUNT];
+distance_table_t tables[TABLE_COUNT];
 
 int fr;
 int previous_distance;
@@ -33,21 +20,15 @@ unsigned long long int previous_page;
 
 void CACHE::l2c_prefetcher_initialize()
 {
-    int i,j;
-    for(i = 0; i < IP_COUNT; i++)
+    int i;
+    for (i = 0; i < TABLE_COUNT; i++)
     {
-        iptable[i].ip = 0;
-        iptable[i].lru = i;
-        iptable[i].previous_a = 0;
-        iptable[i].previous_i = 0;
-        iptable[i].previous_d = 0;
-        iptable[i].fr = 1;
-        //iptable[i].tables[TABLE_COUNT];
-        for(j = 0; j < TABLE_COUNT; j++)
-        {
-            iptable[i].tables[j].distance = 0;
-        }
+        tables[i].tag = -1E06;
+        tables[i].d1 = 0;
+        tables[i].d2 = 0;
+        tables[i].lru = i;
     }
+    fr = 1;
 }
 
 void CACHE::l2c_prefetcher_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit, uint8_t type)
@@ -55,92 +36,9 @@ void CACHE::l2c_prefetcher_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit
     unsigned long long int cl_address = addr >> LOG2_BLOCK_SIZE;
 
     int distance;
-    int index = -1;
-    for(index = 0; index < IP_COUNT; index++)
-    {
-        if(iptable[index].ip == ip)
-        {
-            break;
-        }
-    }
-    
-    //if the table is full
-    if(index == IP_COUNT)
-    {
-        for(index = 0; index < IP_COUNT; index++)
-        {
-            if(iptable[index].lru == (IP_COUNT - 1))
-            {
-                break;
-            }
-        }
-
-        iptable[index].ip = ip;
-        iptable[index].previous_a = cl_address;
-
-        for(int i = 0; i < IP_COUNT; i++)
-        {
-            if(iptable[i].lru < iptable[index].lru)
-            {
-                iptable[i].lru++;
-            }
-        }
-
-        iptable[index].lru = 0;
-    }
-
-    if(index == -1) {assert(0);}
-
-    distance = cl_address - iptable[index].previous_a;
-    if(distance == 0) {return;}
-
-    int d_index = -1;
-    for(d_index = 0; d_index < TABLE_COUNT; d_index ++)
-    {
-        if(iptable[index].tables[d_index].distance == distance)
-        {
-            break;
-        }
-    }
-
-    if(d_index == TABLE_COUNT)
-    {
-        for(d_index = 0; d_index < TABLE_COUNT; d_index++)
-        {
-            //find an empty slot in the table
-            if(iptable[index].tables[d_index].distance == 0)
-            {
-                break;
-            }
-        }
-        //If the table is full
-        if(d_index == TABLE_COUNT) {return;}
-        if(d_index == -1) {assert(0);}
-
-        iptable[index].tables[d_index].distance = distance;
-    }
-
-    int d2;
-    //TODO: Add for loop for prefetch address
-    d2 = iptable[index].tables[d_index + 1].distance;
-    if(d2 == 0) {return;}
-
-    uint64_t pf_address = (cl_address + d2) << LOG2_BLOCK_SIZE;
-    /*if ((pf_address >> LOG2_PAGE_SIZE) != (addr >> LOG2_PAGE_SIZE))
-        {
-            //FIXME: Son't return because we haven't set previous values
-            return;
-        }*/
-    if (MSHR.occupancy < (MSHR.SIZE >> 1))
-    {
-        prefetch_line(ip, addr, pf_address, FILL_L2);
-    }
-    else
-    {
-        prefetch_line(ip, addr, pf_address, FILL_LLC);
-    }
-
-    /*if (!fr)
+    int index;
+    //if(cache_hit == 0) {return;}
+    if (!fr)
     {
         index = -1;
 
@@ -158,11 +56,11 @@ void CACHE::l2c_prefetcher_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit
         //If this distance is not in our table
         if (index == TABLE_COUNT)
         {
-            
-            for(index = 0; index < TABLE_COUNT; index ++)
+
+            for (index = 0; index < TABLE_COUNT; index++)
             {
                 //Find the least recently used row in the table
-                if(tables[index].lru == TABLE_COUNT - 1)
+                if (tables[index].lru == TABLE_COUNT - 1)
                 {
                     break;
                 }
@@ -172,13 +70,14 @@ void CACHE::l2c_prefetcher_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit
             tables[index].tag = distance;
 
             //Increment LRU counter for rows least recently used than evicted row
-            for(int i = 0; i < TABLE_COUNT; i++)
+            for (int i = 0; i < TABLE_COUNT; i++)
             {
-                if(tables[i].lru < tables[index].lru)
+                if (tables[i].lru < tables[index].lru)
                 {
                     tables[i].lru++;
                 }
             }
+            //cout << "Assign " << distance << " new row in " << index << endl;
             tables[index].lru = 0;
             previous_distance = distance;
             previous_addr = cl_address;
@@ -192,6 +91,11 @@ void CACHE::l2c_prefetcher_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit
         }
 
         uint64_t pf_address = (cl_address + tables[index].d1) << LOG2_BLOCK_SIZE;
+        /*if ((pf_address >> LOG2_PAGE_SIZE) != (addr >> LOG2_PAGE_SIZE))
+        {
+            //FIXME: Son't return because we haven't set previous values
+            return;
+        }*/
         if (MSHR.occupancy < (MSHR.SIZE >> 1))
         {
             prefetch_line(ip, addr, pf_address, FILL_L2);
@@ -200,7 +104,8 @@ void CACHE::l2c_prefetcher_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit
         {
             prefetch_line(ip, addr, pf_address, FILL_LLC);
         }
-        
+
+        //cout << "Previous set" << endl;
         tables[previous_index].d1 = distance;
         previous_addr = cl_address;
         previous_distance = distance;
@@ -209,9 +114,10 @@ void CACHE::l2c_prefetcher_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit
     else
     {
         fr = 0;
+        cout << "First run" << fr << endl;
         previous_addr = cl_address;
         //previous_distance = distance;
-    }*/
+    }
 }
 
 void CACHE::l2c_prefetcher_cache_fill(uint64_t addr, uint32_t set, uint32_t way, uint8_t prefetch, uint64_t evicted_addr)
