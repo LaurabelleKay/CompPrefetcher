3314ec5d2595a3722f99da14ce758655f1e180c6
Laurabelle <laurabellek@yahoo.com>
2019-03-19 16:06:20 -0700

    add cache metrics collection

--- next_N.cc
+++ next_N.cc
@@ -7,7 +7,7 @@ typedef struct cache_line
     uint64_t addr;
     uint8_t valid;
     uint8_t pf;
-}cache_line_t;
+} cache_line_t;
 
 cache_line_t cache[4096];
 int cache_size;
@@ -21,7 +21,7 @@ void cache_insert(uint64_t addr, uint8_t pf)
 {
     int i;
     for (i = 0; i < cache_size; i++)
-    //Find an empty slot in the cache
+        //Find an empty slot in the cache
         if (cache[i].valid == 0)
         {
             cache[i].valid = 1;
@@ -51,8 +51,12 @@ int cache_search(uint64_t addr)
 {
     int i;
     for (i = 0; i < cache_size; i++)
+    {
         if (cache[i].addr == addr)
+        {
             return i;
+        }
+    }
     return -1;
 }
 
@@ -64,7 +68,7 @@ void CACHE::l2c_prefetcher_initialize()
 void CACHE::l2c_prefetcher_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit, uint8_t type)
 {
     uint64_t cl_addr = addr >> LOG2_BLOCK_SIZE;
-    for(int i = 1; i < PREFETCH_DEGREE + 1; i++)
+    for (int i = 1; i < PREFETCH_DEGREE + 1; i++)
     {
         uint64_t pf_addr = ((addr >> LOG2_BLOCK_SIZE) + i) << LOG2_BLOCK_SIZE;
         prefetch_line(ip, addr, pf_addr, FILL_L2);
@@ -78,7 +82,7 @@ void CACHE::l2c_prefetcher_operate(uint64_t addr, uint64_t ip, uint8_t cache_hit
     int index = cache_search(addr);
     if (index != -1)
     {
-        if(cache[index].pf == 1)
+        if (cache[index].pf == 1)
         {
             pf_use++;
             cache[index].pf = 0;
@@ -91,7 +95,7 @@ void CACHE::l2c_prefetcher_cache_fill(uint64_t addr, uint32_t set, uint32_t way,
     uint64_t cl_address = addr >> LOG2_BLOCK_SIZE;
     uint64_t evict_addr = addr >> LOG2_BLOCK_SIZE;
     cache_insert(cl_address, prefetch);
-    if(prefetch == 1)
+    if (prefetch == 1)
     {
         pf_count++;
         int index = cache_search(evict_addr);
@@ -100,5 +104,4 @@ void CACHE::l2c_prefetcher_cache_fill(uint64_t addr, uint32_t set, uint32_t way,
 
 void CACHE::l2c_prefetcher_final_stats()
 {
-
 }
